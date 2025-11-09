77e9da4fd3e39e712095d3f04d324f723c2ab8c7
muzammilmehdi172-hue <muzammilmehdi172@gmail.com>
2025-11-09 21:28:21 +0500

    Refactor app.py for model downloading and enhancement

--- app.py
+++ app.py
@@ -122,6 +122,6 @@ def uploaded_file(filename):
 def result_file(filename):
     return send_from_directory(app.config['RESULT_FOLDER'], filename)
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
