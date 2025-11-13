45354c17eead742f0bf8f4d15b38717184cbffcd
muzammilmehdi172-hue <muzammilmehdi172@gmail.com>
2025-11-13 18:16:44 +0500

    Update app.py

--- app.py
+++ app.py
@@ -132,6 +132,8 @@ def results(path):
 def home():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host='0.0.0.0', port=port)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
